b45008853d88528a955fa105ac566e04924b40f6
Lucas B <lucasbattelle@gmail.com>
2018-06-21 12:02:52 -0700

    No Gui but now threaded. Can run two timers at once

--- Timer.py
+++ Timer.py
@@ -1,20 +1,19 @@
-import sys
-from tkinter import *
 import threading
-from queue import Queue
 import time
 
 
-class Timer:
+class Timer (threading.Thread):
 
     def __init__(self, name, length):
+        threading.Thread.__init__(self)
         self.name = name
         self.length = length
 
+        
     def __str__(self):
         return "{} for {}".format(self.name, self.length)
 
-    def start(self):
+    def run(self):
         print("Timer {} for {} seconds is started".format(self.name, self.length))
 
         for x in range(0, self.length):
@@ -23,26 +22,5 @@ class Timer:
         print("Timer {} for {} seconds is finished".format(self.name, self.length))
 
 
-def main():
-    root = Tk()
-    if len(sys.argv) > 2:
-        test = Timer(sys.argv[1], int(sys.argv[2]))
-        message = Label(root, text=str(test))
-        message.pack()
-
-        root.update()
-        test.start()
-        message = Label(root, text="Finished")
-        message.pack()
-        while True:
-            root.update()
-
-    else:
-        message = Label(root, text="Please include the name and duration of the timer in the arguments")
-        message.pack()
-        while True:
-            root.update()
-
-
-if __name__ == "__main__":
-    main()
+Timer("test1", 5).start()
+Timer("test2", 7).start()
